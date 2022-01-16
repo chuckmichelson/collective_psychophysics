53f79d96d738112fca0f4df5014975907ca213a6
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-16 14:06:16 -0600

    testing

--- constants.js
+++ constants.js
@@ -11,6 +11,7 @@ const CTR_POS_X = 256;
 const CTR_POS_Y = 256;
 
 const OUIJA_CODES = {
+    0 : {letter : '0', x : CTR_POS_X + STIMULI_DISTANCE * Math.cos(90), y : CTR_POS_Y + STIMULI_DISTANCE * Math.sin(90) },
     1 : {letter : '1', x : CTR_POS_X + STIMULI_DISTANCE * Math.cos(60), y : CTR_POS_Y + STIMULI_DISTANCE * Math.sin(60) },
     2 : {letter : '2', x : CTR_POS_X + STIMULI_DISTANCE * Math.cos(30), y : CTR_POS_Y + STIMULI_DISTANCE * Math.sin(30) },
     3 : {letter : '3', x : CTR_POS_X + STIMULI_DISTANCE * Math.cos(0), y : CTR_POS_Y + STIMULI_DISTANCE * Math.sin(0) },
@@ -20,9 +21,8 @@ const OUIJA_CODES = {
     7 : {letter : '7', x : CTR_POS_X + STIMULI_DISTANCE * Math.cos(-120), y : CTR_POS_Y + STIMULI_DISTANCE * Math.sin(-120) },
     8 : {letter : '8', x : CTR_POS_X + STIMULI_DISTANCE * Math.cos(-150), y : CTR_POS_Y + STIMULI_DISTANCE * Math.sin(-150) },
     9 : {letter : '9', x : CTR_POS_X + STIMULI_DISTANCE * Math.cos(-180), y : CTR_POS_Y + STIMULI_DISTANCE * Math.sin(-180) },
-    10 : {letter : 'A', x : CTR_POS_X + STIMULI_DISTANCE * Math.cos(-210), y : CTR_POS_Y + STIMULI_DISTANCE * Math.sin(-210) },
-    11 : {letter : 'B', x : CTR_POS_X + STIMULI_DISTANCE * Math.cos(-240), y : CTR_POS_Y + STIMULI_DISTANCE * Math.sin(-240) },
-    12 : {letter : 'C', x : CTR_POS_X + STIMULI_DISTANCE * Math.cos(-270), y : CTR_POS_Y + STIMULI_DISTANCE * Math.sin(-270) },
+    10 : {letter : '10', x : CTR_POS_X + STIMULI_DISTANCE * Math.cos(-210), y : CTR_POS_Y + STIMULI_DISTANCE * Math.sin(-210) },
+    11 : {letter : '11', x : CTR_POS_X + STIMULI_DISTANCE * Math.cos(-240), y : CTR_POS_Y + STIMULI_DISTANCE * Math.sin(-240) },
 }
 
 
