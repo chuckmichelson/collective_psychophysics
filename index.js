acd2dd336b27341e71a62b29c3f18f5635df8f1b
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-16 04:27:15 -0600

    starting collective-psychophysics

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const PLANCHETTE_WIDTH = 120;
 const PLANCHETTE_HEIGHT = 120;
 
 // ***** CHANGE THIS TO RUN ON HEROKU
-// const socket = io('http://localhost:3000');
-const socket = io('https://collective-psychophysics.herokuapp.com/');
+const socket = io('http://localhost:3000');
+// const socket = io('https://collective-psychophysics.herokuapp.com/');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
