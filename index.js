e2b233d77055a0fd6fac798d3cd5032a6428d861
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-16 22:56:39 -0600

    testing

--- index.js
+++ index.js
@@ -48,11 +48,11 @@ console.log("sockets on")
 document.body.style.backgroundColor = "black";
 // setTimeout(() => { collapseSplash(); joinGame();}, 2000);
 
-var btn = document.getElementById('blurBtn');
+// var btn = document.getElementById('blurBtn');
 var img = document.getElementById('blurImg');
 img.style.display = "none";
 
-btn.addEventListener('click', addBlur)
+// btn.addEventListener('click', addBlur)
 
 function addBlur() {
   img.style.filter = 'blur(10px)';
