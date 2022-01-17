376d7279acbe0d5f4a6b6da05821c21bb5fd5538
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-17 11:08:59 -0600

    testing

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const CLOCK_POSITIONS = {
 }
 
 // ***** CHANGE THIS TO RUN ON HEROKU
-const socket = io('http://localhost:3000');
-// const socket = io('https://collective-psychophysics.herokuapp.com/');
+// const socket = io('http://localhost:3000');
+const socket = io('https://collective-psychophysics.herokuapp.com/');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
