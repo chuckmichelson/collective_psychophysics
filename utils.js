a5eda22c45607067cff555042d5f1bdf6bebd7f1
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-17 14:35:23 -0600

    foveation

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 const { FRAME_RATE } = require('./constants');
-const { CLOCK_POSITIONS } = require('./constants');
+// const { CLOCK_POSITIONS } = require('./constants');
 
 
 module.exports = {
@@ -20,40 +20,40 @@ function makeid(length) {
 }
 
 function ouijaGoToLetter(state, letter) {
-   // console.log("made it to ouijaGoToLetter")
-   // xstart = state.planchette.pos.x;
-   // xend = CLOCK_POSITIONS[letter].x;
-   // ystart = state.planchette.pos.y;
-   // yend = CLOCK_POSITIONS[letter].y;
-   // for ( var i = 0; i < 100; i++ ) {
+  //  // console.log("made it to ouijaGoToLetter")
+  //  // xstart = state.planchette.pos.x;
+  //  // xend = CLOCK_POSITIONS[letter].x;
+  //  // ystart = state.planchette.pos.y;
+  //  // yend = CLOCK_POSITIONS[letter].y;
+  //  // for ( var i = 0; i < 100; i++ ) {
 
-   // }
-   ouijaAlphabetLength = Object.keys(CLOCK_POSITIONS).length;
-   for ( var i = 0; i < ouijaAlphabetLength - 1; i++ ) {
-      if (CLOCK_POSITIONS[i].letter === letter) {
-         state.planchette.pos.x = CLOCK_POSITIONS[i].x
-         state.planchette.pos.y = CLOCK_POSITIONS[i].y
-      }
-   }
-  return state;
+  //  // }
+  //  ouijaAlphabetLength = Object.keys(CLOCK_POSITIONS).length;
+  //  for ( var i = 0; i < ouijaAlphabetLength - 1; i++ ) {
+  //     if (CLOCK_POSITIONS[i].letter === letter) {
+  //        state.planchette.pos.x = CLOCK_POSITIONS[i].x
+  //        state.planchette.pos.y = CLOCK_POSITIONS[i].y
+  //     }
+  //  }
+  // return state;
 }
 
 function ouijaGetLetter(state) {
    letter = '_';
-   ouijaAlphabetLength = Object.keys(CLOCK_POSITIONS).length;
+   // ouijaAlphabetLength = Object.keys(CLOCK_POSITIONS).length;
    // console.log("ouijaAlphabetLength: " + ouijaAlphabetLength);
    // console.log("state.planchette.pos.x: " + state.planchette.pos.x);
    posx = state.planchette.pos.x;
    posy = state.planchette.pos.y;
 
-   for ( var i = 0; i < ouijaAlphabetLength; i++ ) {
+   for ( var i = 0; i < 1; i++ ) {
       // console.log(i)
-      codx = CLOCK_POSITIONS[i].x;
-      cody = CLOCK_POSITIONS[i].y;
+      codx = 256;
+      cody = 256;
       distance = Math.sqrt(Math.pow(codx - posx, 2) + Math.pow(cody - posy, 2));
       // console.log(distance);
       if (distance <= 15) {
-         letter = CLOCK_POSITIONS[i].letter;
+         letter = '1';
          // console.log('Your letter is: ' + letter);
       }
    }
