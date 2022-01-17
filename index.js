70d806aa714c2d0d687de479813140601f2e5ce9
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-17 00:22:47 -0600

    testing

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function init() {
   imgClip = new Image();
   imgClip.src = "images/fovea_mask.png";
   layer1.style.filter = "blur(10px)";
-  ctx1.drawImage(search_image, 0, 0);
+  ctx1.drawImage(imgClip, 0, 0);
 
 
   // /// draw the shape we want to use for clipping
