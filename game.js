0b985ef919651a262048ef49174941798334b173
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-17 23:53:37 -0600

    add initial stimuli

--- game.js
+++ game.js
@@ -170,7 +170,7 @@ function getUpdatedVelocity(keyCode) {
 
 
 function makeTrial(state) {
-  state.current_trial.stimulus = STIMULI[4];
+  state.current_trial.stimulus = STIMULI[5];
   state.current_trial.blur = 16;
   state.triggerNewTrial = true;
   return state;
