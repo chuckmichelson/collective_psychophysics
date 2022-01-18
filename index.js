060175d76557e70431c89a55f79ef5707d518b8b
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-17 21:48:43 -0600

    add initial stimuli

--- index.js
+++ index.js
@@ -91,13 +91,13 @@ let playerNumber = 1;
 let gameActive = false;
 
 
-function init(state) {
+function init() {
 
   const layer1 = document.getElementById('layer1');
   const ctx1 = layer1.getContext('2d');
   search_image = new Image();
   search_image.src = "images/animal_000.jpg";
-  console.log(state.current_trial.stimulus.image_path);
+  // search_image.src = state.current_trial.stimulus.image_path;
   layer1.style.filter = "blur(10px)";
   ctx1.drawImage(search_image, 0, 0);
 
@@ -132,14 +132,6 @@ function keyDown(e) {
 }
 
 function paintGame(state) {
-
-  const layer1 = document.getElementById('layer1');
-  const ctx1 = layer1.getContext('2d');
-  search_image = new Image();
-  search_image.src = "images/animal_000.jpg";
-  // search_image.src = state.current_trial.stimulus.image_path;
-  layer1.style.filter = "blur(10px)";
-  ctx1.drawImage(search_image, 0, 0);
 
   // display fovea
   const layer2 = document.getElementById('layer2');
