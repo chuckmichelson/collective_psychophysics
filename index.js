112a099e76dda44a75f72f3049a415c846845d7d
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-19 12:25:33 -0600

    about

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ document.body.style.backgroundColor = "black";
 // setTimeout(() => { collapseSplash(); joinGame();}, 2000);
 
 // var btn = document.getElementById('blurBtn');
-var img = document.getElementById('blurImg');
-img.style.display = "none";
+// var img = document.getElementById('blurImg');
+// img.style.display = "none";
 
 // const layer1 = document.getElementById('layer1');
 // const ctx1 = layer1.getContext('2d');
