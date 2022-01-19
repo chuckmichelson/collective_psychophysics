f49c469cf29fccba090bbcd580cbbfb58fc4ee9b
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-19 09:50:46 -0600

    about

--- game.js
+++ game.js
@@ -172,7 +172,7 @@ function getUpdatedVelocity(keyCode) {
 
 
 function makeTrial(state) {
-  state.current_trial.stimulus = STIMULI[5];
+  state.current_trial.stimulus = STIMULI[3 + 1];
   state.current_trial.blur = BLUR[Math.floor(Math.random() * BLUR.length)];
   console.log(STIMULI[0].length)
   state.triggerNewTrial = true;
